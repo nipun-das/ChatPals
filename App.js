7fa36d468746c9a9bf73e8819025ab171525637c
C C Nipun Das <nipundas02@gmail.com>
2023-02-17 19:54:34 +0530

    final codes

--- App.js
+++ App.js
@@ -1,20 +1,91 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useState, createContext, useContext } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Chat from './screens/Chat';
+import Login from './screens/Login';
+import Signup from './screens/Signup';
+import Home from './screens/Home';
+import { auth } from './config/firebase';
+import { ActivityIndicator } from 'react-native';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useEffect } from 'react';
+
+
+
+
+const Stack = createStackNavigator();
+const AuthenticatedUserContext = createContext({});
+
+const AuthenticatedUserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <AuthenticatedUserContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthenticatedUserContext.Provider>
+  )
+}
+
+
+
+
+function ChatStack() {
+  return (
+    <Stack.Navigator defaultScreenOptions={Home} >
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Chat" component={Chat} />
+    </Stack.Navigator>
+  )
+}
+
+function AuthStack() {
+  return (
+
+    <Stack.Navigator defaultScreenOptions={Login} screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Signup" component={Signup} />
+    </Stack.Navigator>
+  )
+
+}
+
+
+function RootNavigator() {
+  const { user, setUser } = useContext(AuthenticatedUserContext);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth,
+      async authenticatedUser => {
+        authenticatedUser ? setUser(authenticatedUser) : setUser(null);
+        setLoading(false);
+      }
+    );
+    return () => unsubscribe();
+  }, [user]);
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size='large' />
+      </View>
+    );
+  }
+
+
+  return (
+    <NavigationContainer>
+      {user ? <ChatStack /> : <AuthStack />}
+    </NavigationContainer>
+  )
+}
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    <AuthenticatedUserProvider><RootNavigator /></AuthenticatedUserProvider>)
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
