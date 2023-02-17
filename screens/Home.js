582e0cfac8b8e90380ec104a9fbf221db46dd143
C C Nipun Das <nipundas02@gmail.com>
2023-02-17 20:24:52 +0530

    icons and images

--- screens/Home.js
+++ screens/Home.js
@@ -4,7 +4,7 @@ import { useNavigation } from "@react-navigation/native";
 import { FontAwesome } from '@expo/vector-icons';
 import colors from '../colors';
 import { Entypo } from '@expo/vector-icons';
-const catImageUrl = "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d";
+const catImageUrl = "https://cdn-icons-png.flaticon.com/512/924/924874.png";
 
 const Home = () => {
 
@@ -19,8 +19,8 @@ const Home = () => {
                 <Image
                     source={{ uri: catImageUrl }}
                     style={{
-                        width: 40,
-                        height: 40,
+                        width: 35,
+                        height: 35,
                         marginRight: 15,
                     }}
                 />
