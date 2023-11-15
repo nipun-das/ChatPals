52501912a527044c9126b3ed2a218c652fc343e9
nipun-das <nipundas02@gmail.com>
2023-11-15 11:20:40 +0530

    auth

--- screens/ClubCreationSuccess.js
+++ screens/ClubCreationSuccess.js
@@ -1,14 +1,39 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useEffect } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+
+
+
+
 
 function ClubCreationSuccess() {
+
+    useEffect(() => {
+        // Check if there is a currently authenticated user
+        console.log("hi")
+    }, []);
     return (
 
         <View>
-            <Text>Sucessfully created club</Text>
+            <Text style={styles.label}>Sucessfully created club</Text>
         </View>
 
     )
 }
 
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 50,
+        flex: 1,
+        backgroundColor:'yellow',
+        padding: 16,
+    },
+    label: {
+        fontSize: 50,
+        color:'blue',
+        fontWeight: 'bold',
+        marginBottom: 8,
+    },
+
+});
 export default ClubCreationSuccess
