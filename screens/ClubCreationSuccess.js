dd4ed7883e53349b64759b703a3a1af232091e48
nipun-das <nipundas02@gmail.com>
2023-11-15 14:29:43 +0530

    creation

--- screens/ClubCreationSuccess.js
+++ screens/ClubCreationSuccess.js
@@ -1,39 +1,88 @@
-import React, { useEffect } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
 
 
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-
-
-
-function ClubCreationSuccess() {
+function ClubCreationSuccess({ route }) {
+    const { clubName } = route.params || {};
+    console.log(route.params);
 
     useEffect(() => {
-        // Check if there is a currently authenticated user
-        console.log("hi")
+        console.log("recv", clubName)
     }, []);
+
     return (
+        <View style={styles.container}>
 
-        <View>
-            <Text style={styles.label}>Sucessfully created club</Text>
+            <View style={styles.curvedBg} />
+            <SafeAreaView style={styles.form}>
+                <View style={styles.header}>
+                    <Image source={require('../assets/star.png')} style={styles.image} resizeMode="contain" />
+                    <View style={styles.titleContainer}>
+                        <Text style={styles.title}>Hooray!🎈</Text>
+                    </View>
+                </View>
+            </SafeAreaView>
+
+            <Text style={styles.subLabel}>Club Name: {clubName}</Text>
         </View>
-
-    )
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
-        marginTop: 50,
         flex: 1,
-        backgroundColor:'yellow',
-        padding: 16,
-    },
-    label: {
-        fontSize: 50,
-        color:'blue',
-        fontWeight: 'bold',
-        marginBottom: 8,
+        backgroundColor: "#fff",
     },
 
+    form: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        marginHorizontal: 30,
+        fontFamily: 'Poppins-Regular'
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 40,
+        marginTop: 50
+    },
+    image: {
+        width: 40, // Set your desired width
+        height: 40, // Set your desired height
+        position: 'absolute',
+        top: 10,
+        right: 0,
+    },
+    titleContainer: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 45,
+        // fontWeight: 'bold',
+        color: 'black',
+        marginTop: 18,
+        fontFamily: 'Poppins-Bold',
+    },
+    bgImage: {
+        width: "100%",
+        height: 340,
+        position: "absolute",
+        opacity: 0.85,
+        top: 0,
+        resizeMode: 'cover',
+    },
+    curvedBg: {
+        width: '100%',
+        height: '75%',
+        position: "absolute",
+        bottom: 0,
+        backgroundColor: '#fff',
+        borderTopLeftRadius: 60,
+    },
 });
-export default ClubCreationSuccess
+
+export default ClubCreationSuccess;
